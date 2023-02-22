893fe1bfd2e16ef775d27a3e3290b8070c51c72a
Abhishek kumar <ak749258@gmail.com>
2023-02-22 11:13:57 +0530

    Update2

--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -11,7 +11,7 @@ router.post('/register', async (req, res) => {
         })
 
     try {
-        await uplicuser.checkDates(user.username,user.email)
+        await user.checkDuplicates(user.username,user.email)
         const token = await user.generateAuthToken()
         await user.save()
         await profile.save()
--- p.js
+++ p.js
@@ -13,3 +13,4 @@
 //         }
 //     ]
 // }
+//"./backend/public/index.html"
